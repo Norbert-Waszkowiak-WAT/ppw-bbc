710eadd87321b1480bf613b4041a2305ac680092
MidoParaX <145282032+MidoParaX@users.noreply.github.com>
2023-09-22 21:19:19 +0200

    Add files via upload

--- Gra.py
+++ Gra.py
@@ -4,21 +4,29 @@ import random
 import random
 import tkinter as tk
 from tkinter import messagebox
+import os
 
+WHITE = (255, 255, 255)
 
-def draw_grid(w, rows, surface):
-    size_between = w // rows
-    x = 0
-    y = 0
-    for i in range(rows):
-        x += size_between
-        y += size_between
-        pg.draw.line(surface, (225, 225, 225), (x, 0), (x, w))
-        pg.draw.line(surface, (225, 225, 225), (0, y), (w, y))
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 700
+window = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+VEL = 5 # velocity
+BOSS_WIDTH = 80
+BOSS_HEIGHT = 150
+BOSS = pg.image.load(os.path.join('pliki', 'boss.png'))
+BOSS = pg.transform.scale(BOSS, (BOSS_WIDTH, BOSS_HEIGHT))
 
-def show_screen(surface):
-    draw_grid(size, rows, surface)
+PLAYER_WIDTH = 80
+PLAYER_HEIGHT = 150
+PLAYER = pg.image.load(os.path.join('pliki', 'player.png'))
+PLAYER = pg.transform.scale(PLAYER, (PLAYER_WIDTH, PLAYER_HEIGHT))
+
+def draw_window(PLAYER_POSITION, BOSS_POSITION):
+    window.fill(WHITE)
+    window.blit(BOSS, (BOSS_POSITION.x, BOSS_POSITION.y))
+    window.blit(PLAYER, (PLAYER_POSITION.x, PLAYER_POSITION.y))
     pg.display.update()
 
 
@@ -28,13 +36,13 @@ class Player(object):
 
 
 def main():
-    global size, rows
-    size = 700 #size of screen
-    rows = 20
+    PLAYER_POSITION = pg.Rect(500, 300, PLAYER_WIDTH, PLAYER_HEIGHT)
+    BOSS_POSITION = pg.Rect(500, 50, BOSS_WIDTH, BOSS_HEIGHT)
 
-    window = pg.display.set_mode((size, size))
 
-    p = Player((0, 0, 0), (10, 10))
+
+
+
 
     game = True
 
@@ -43,10 +51,19 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 game = False
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[pg.K_a]:
+            PLAYER_POSITION.x -= VEL
+        if keys_pressed[pg.K_d]:
+            PLAYER_POSITION.x += VEL
+        if keys_pressed[pg.K_s]:
+            PLAYER_POSITION.y += VEL
+        if keys_pressed[pg.K_w]:
+            PLAYER_POSITION.y -= VEL
 
-        pg.time.delay(50) #opóźnienie w miliseundach
 
-        clock.tick(10) #ilość klatek na sekunde
 
-        show_screen(window)
+        clock.tick(60) #ilość klatek na sekunde
+
+        draw_window(PLAYER_POSITION, BOSS_POSITION)
 main()
