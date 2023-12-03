904ecc22f1faaecc1833cff8b242de70ce0576f5
Ksawery Pijet <145845447+maszubin@users.noreply.github.com>
2023-12-03 22:42:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -40,8 +40,7 @@ class Game:
         self.weapons = pygame.sprite.LayeredUpdates()
 
         self.i = 0
-        self.KYS = "Ksawery"
-        self.kys = "Miłosz"
+
 
     def createTilemap(self):
         x = 0
